62e2333665115e782840f8eabd8c53c278b7d40d
YongPilMoon <m71661@gmail.com>
2017-05-18 14:37:56 +0900

    Setting hbs view engine

---
+++ server/app.js
@@ -0,0 +1,24 @@
+var path = require('path');
+const express = require('express');
+const hbs = require('express-hbs');
+
+var app = express();
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'/views'));
+app.engine('hbs', hbs.express4({
+    partialsDir: path.join(__dirname,'/views/partials'),
+    layoutsDir: path.join(__dirname,'/views/layouts')
+}));
+
+
+
+app.use(express.static(__dirname + '/public'));
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.render('index.hbs');
+});
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
