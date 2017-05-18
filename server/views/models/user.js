97078694ca259e56816d500da6ce7b9c2c01ce65
YongPilMoon <m71661@gmail.com>
2017-05-18 15:24:09 +0900

    Add User model

---
+++ server/views/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
+var UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+});
+
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else{
+        next();
+    }
+});
+
+var User = mongoose.model('user', UserSchema);
+
+module.exports = {User};
